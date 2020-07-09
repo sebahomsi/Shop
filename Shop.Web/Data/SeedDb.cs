526b33f2259c94269653daa6c1d101bd7d8a08ff
Sebastian <sebahomsi@gmail.com>
2020-07-09 17:15:50 -0300

    autenticacion personalizada

--- Shop.Web/Data/SeedDb.cs
+++ Shop.Web/Data/SeedDb.cs
@@ -1,4 +1,6 @@
-﻿namespace Shop.Web.Data
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Shop.Web.Data
 {
     using System;
     using System.Linq;
@@ -8,34 +10,56 @@
     {
         private readonly DataContext context;
         private Random random;
+        private readonly UserManager<User> userManager;
 
-        public SeedDb(DataContext context)
+        public SeedDb(DataContext context, UserManager<User> userManager)
         {
             this.context = context;
             this.random = new Random();
+            this.userManager = userManager;
         }
 
         public async Task SeedAsync()
         {
             await this.context.Database.EnsureCreatedAsync();
 
+            var user = await this.userManager.FindByEmailAsync("sebita@gmail.com");
+            if (user == null)
+            {
+                user = new User
+                {
+                    FirstName = "Seba",
+                    LastName = "Homsi",
+                    Email = "sebita@gmail.com",
+                    UserName = "sebita@gmail.com"
+                };
+
+                var result = await this.userManager.CreateAsync(user, "123456");
+                if (result != IdentityResult.Success)
+                {
+                    throw new InvalidOperationException("Could not create the user in seeder");
+                }
+            }
+
             if (!this.context.Products.Any())
             {
-                this.AddProduct("iPhone XS");
-                this.AddProduct("Samsung S10");
-                this.AddProduct("Play Station 5");
+                this.AddProduct("First Product", user);
+                this.AddProduct("Second Product", user);
+                this.AddProduct("Third Product", user);
                 await this.context.SaveChangesAsync();
             }
         }
 
-        private void AddProduct(string name)
+
+        private void AddProduct(string name, User user)
         {
             this.context.Products.Add(new Product
             {
                 Name = name,
                 Price = this.random.Next(500),
                 IsAvailable = true,
-                Stock = this.random.Next(50)
+                Stock = this.random.Next(50),
+                User = user
             });
         }
 
--- Shop.Web/Startup.cs
+++ Shop.Web/Startup.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Shop.Web.Data;
+using Shop.Web.Data.Entities;
 
 namespace Shop.Web
 {
@@ -21,6 +23,19 @@ namespace Shop.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            //Configuracion de usuarios
+            services.AddIdentity<User, IdentityRole>(cfg =>
+            {
+                cfg.User.RequireUniqueEmail = true;
+                cfg.Password.RequireDigit = false;
+                cfg.Password.RequiredUniqueChars = 0;
+                cfg.Password.RequireLowercase = false;
+                cfg.Password.RequireNonAlphanumeric = false;
+                cfg.Password.RequireUppercase = false;
+                cfg.Password.RequiredLength = 6;
+            })
+            .AddEntityFrameworkStores<DataContext>();
 
             //Inyeccion de la base de datos
             services.AddDbContext<DataContext>(cfg =>
@@ -62,6 +77,7 @@ namespace Shop.Web
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseCookiePolicy();
 
             app.UseMvc(routes =>
